c067a36cfdab4d62485c17140c8ebc5c19e1bcc9
Raul Sanchez <rawmind@gmail.com>
2016-06-20 22:07:17 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM rawmind/alpine-jvm8:1.8.92-1
+MAINTAINER Raul Sanchez <rawmind@gmail.com>
+
+# Set environment
+ENV SERVICE_HOME=/opt/kafka \
+    SERVICE_NAME=kafka \
+    SCALA_VERSION=2.11 \
+    SERVICE_VERSION=0.9.0.1 \
+    SERVICE_USER=kafka \
+    SERVICE_UID=10002 \
+    SERVICE_GROUP=kafka \
+    SERVICE_GID=10002 \
+    SERVICE_URL=http://apache.mirrors.spacedump.net/kafka
+ENV SERVICE_RELEASE=kafka_"$SCALA_VERSION"-"$SERVICE_VERSION"  
+
+# Install and configure kafka
+RUN curl -sS -k ${SERVICE_URL}/${SERVICE_VERSION}/${SERVICE_RELEASE}.tgz | gunzip -c - | tar -xf - -C /opt \
+  && ln -s /opt/${SERVICE_RELEASE} ${SERVICE_HOME} \
+  && cd ${SERVICE_HOME}/libs/ \
+  && mkdir ${SERVICE_HOME}/data ${SERVICE_HOME}/logs \
+  && addgroup -g ${SERVICE_GID} ${SERVICE_GROUP} \
+  && adduser -g "${SERVICE_NAME} user" -D -h ${SERVICE_HOME} -G ${SERVICE_GROUP} -s /sbin/nologin -u ${SERVICE_UID} ${SERVICE_USER} 
+
+ADD root /
+RUN chmod +x ${SERVICE_HOME}/bin/*.sh \
+  && chown -R ${SERVICE_USER}:${SERVICE_GROUP} ${SERVICE_HOME} /opt/monit
+
+
+USER $SERVICE_USER
+WORKDIR $SERVICE_HOME
+
+EXPOSE 9092
---
+++ root/opt/kafka/bin/server.properties.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+if [ "$KAFKA_EXT_IP" == "" ]; then
+ 	export KAFKA_ADVERTISE_LISTENER=${KAFKA_ADVERTISE_LISTENER:-"${KAFKA_LISTENER}"}
+else
+	export KAFKA_ADVERTISE_LISTENER=${KAFKA_ADVERTISE_LISTENER:-"${KAFKA_LISTENER},PLAINTEXT://${KAFKA_EXT_IP}:${KAFKA_ADVERTISE_PORT}"}
+fi
+
+cat << EOF > ${SERVICE_CONF}
+############################# Server Basics #############################
+broker.id=0
+############################# Socket Server Settings #############################
+listeners=${KAFKA_LISTENER}
+advertised.listeners=${KAFKA_ADVERTISE_LISTENER}
+num.network.threads=3
+num.io.threads=8
+socket.send.buffer.bytes=102400
+socket.receive.buffer.bytes=102400
+socket.request.max.bytes=104857600
+############################# Log Basics #############################
+log.dirs=${KAFKA_LOG_DIRS}
+num.partitions=${KAFKA_NUM_PARTITIONS}
+num.recovery.threads.per.data.dir=1
+############################# Log Flush Policy #############################
+#log.flush.interval.messages=10000
+#log.flush.interval.ms=1000
+############################# Log Retention Policy #############################
+log.retention.hours=${KAFKA_LOG_RETENTION_HOURS}
+#log.retention.bytes=1073741824
+log.segment.bytes=1073741824
+log.retention.check.interval.ms=300000
+log.cleaner.enable=true
+############################# Connect Policy #############################
+zookeeper.connect=${KAFKA_ZK_HOST}:2181
+zookeeper.connection.timeout.ms=6000
+EOF
+
